b43ce1f0666ee68686a90b408ef3c0226d3eba15
apple <24100280@lums.edu.pk>
2026-03-16 23:06:38 +0500

    fail test for countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,5 +22,17 @@ public class CustomListTest {
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
     }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
 
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+
+        City toronto = new City("Toronto", "ON");
+        list.addCity(toronto);
+        assertEquals(2, list.countCities());
+    }
 }
